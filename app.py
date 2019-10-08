cdf40f8d8a1eba66d87bdd13a3499063863a89ad
Mohamed Rageh <mhmdrgh@gmail.com>
2019-10-08 17:34:15 +0200

    basic structure app

--- app.py
+++ app.py
@@ -2,14 +2,33 @@ from flask import Flask, render_template, session, flash, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 @app.route('/')
 def index():
@@ -27,9 +46,49 @@ def quick_form():
         return redirect(url_for('quick_form'))
     return render_template('quick-form.html', form=form, name=session.get('name'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login_form():
+    loginform = LoginForm()
+    if loginform.validate_on_submit():
+        user = User.query.filter_by(username=loginform.username.data, password=loginform.password.data).first()
+        if user is None:
+            return '<h3>Invalid Username or Password</h3>'
+        else:
+            session['known'] = True
+        session['username'] = loginform.username.data
+        loginform.username.data = ''
+        return redirect(url_for('login_form'))
+    return render_template('login-form.html', loginform = loginform, username = session.get('username'), known = session.get('known', False))
+
 Form = FlaskForm
+login_form = FlaskForm
+
 class NameForm(Form):
     name = StringField('Your name', validators=[DataRequired()])
+
+class LoginForm(Form):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField()
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    roleName = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(64))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ == "__main__":
     app.run(debug=True) 
