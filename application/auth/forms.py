2e95fbb73ee07f194f9c98903d9ebf8c9f3fb7a5
Mohamed Rageh <mhmdrgh@gmail.com>
2019-10-11 16:22:19 +0200

    none

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, Length, Regexp, EqualTo
 from wtforms import ValidationError
-from .models import User
+from ..models import User
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
