bd3963557ad3401900ab8bcd01bfd93d5de437ca
= <=>
2019-10-24 09:19:19 +0200

    register with confirmation mail

--- application/models.py
+++ application/models.py
@@ -3,6 +3,7 @@ from flask import current_app, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -19,6 +20,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
     
     @property
     def password(self):
@@ -32,6 +34,24 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    confirmed = db.Column(db.Boolean, default=False)
+    
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+    
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
