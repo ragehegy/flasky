d71ef2277d012fb426641e0262edd3d00da16b5f
= <=>
2019-10-25 13:35:33 +0200

    email credentials added, auth views functionss

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'Mohamed Rageh <mhmdrgh@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     
     @staticmethod
@@ -19,8 +19,10 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'mhmdrgh@gmail.com'
+    MAIL_PASSWORD = '22119966'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
